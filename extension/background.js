82e1c9ef7995121df953456841fdd9b4faea4174
Alan Baines <abaines.vt+github@gmail.com>
2025-08-12 16:50:27 -0400

    ✍ Refactor badge text assignment to handle zero count case correctly

--- extension/background.js
+++ extension/background.js
@@ -3,7 +3,7 @@
 
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
     if (msg.type === 'SET_BADGE_COUNT' && typeof msg.count === 'number') {
-        let text = msg.count > 999 ? '999+' : msg.count > 0 ? String(msg.count) : '';
+        const text = msg.count > 999 ? '999+' : String(msg.count);
         chrome.action.setBadgeText({ text });
         chrome.action.setBadgeBackgroundColor({ color: '#FFA500' }); // Orange badge
         console.log('[background.js] Received SET_BADGE_COUNT:', msg.count, 'Setting badge text to:', text);
